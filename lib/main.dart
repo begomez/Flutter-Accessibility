e30b1a453ca719a44efd54cd6d3951783e3b10f9
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-12-14 13:58:58 +0100

    Remove semantic props

--- lib/main.dart
+++ lib/main.dart
@@ -44,9 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(
           widget.title,
-          semanticsLabel: widget.title,
         ),
-        excludeHeaderSemantics: false,
       ),
       body: Center(
         child: Column(
