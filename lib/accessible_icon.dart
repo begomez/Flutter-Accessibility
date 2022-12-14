f97355bc96f9bd4f18ba71885dfb24f7a14429cf
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-12-14 14:16:33 +0100

    Miinor changes

--- lib/accessible_icon.dart
+++ lib/accessible_icon.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 class AccessibleIcon extends StatelessWidget {
   final IconData iconData;
   final String descrip;
+  final double size;
 
-  const AccessibleIcon({required this.iconData, required this.descrip, Key? key}) : super(key: key);
+  const AccessibleIcon({required this.iconData, required this.descrip, required this.size, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Icon(
       semanticLabel: descrip,
       iconData,
-      size: 100,
+      size: size,
     );
   }
 }
